0db3692f28e10986f69325685cb5816443e7cf90
nariksan <narik-san@mail.ru>
2026-05-19 10:42:21 +1100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,7 @@
-self.addEventListener('install', (e) => { console.log('[SW] Install'); });
-self.addEventListener('fetch', (e) => {});
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+});
+
+self.addEventListener('fetch', (e) => {
+  // Pass through
+});
